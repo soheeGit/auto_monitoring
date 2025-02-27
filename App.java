a550d1e5aa5026d74afbec54e35eb22351588156
진소희 <130043092+soheeGit@users.noreply.github.com>
2025-02-27 15:38:12 +0900

    이름

--- App.java
+++ App.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Solution05 {
+public class App {
     public static void main(String[] args) {
         Monitoring monitoring = new Monitoring();
         monitoring.getNews(System.getenv("KEYWORD"), 10, 1, SortType.date);
